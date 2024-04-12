9aa5be7b8d586fd96bcf87d6c5cb81088df2bb8a
AshbornXS <brutalivio4@gmail.com>
2024-04-12 16:19:09 -0300

    the API can now run headless

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 const express = require('express');
-const { Sequelize, DataTypes } = require('sequelize');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Inicialize o Express
 const app = express();
 app.use(express.json());
 
-// Inicialize Sequelize
-const sequelize = new Sequelize('siga', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql'
+// Conecte-se ao MongoDB
+mongoose.connect(process.env.MONGODB_URI);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Conectado ao MongoDB!");
 });
 
-// Defina um modelo para a tabela alunos
-const Aluno = sequelize.define('Aluno', {
-  nome: DataTypes.STRING,
-  RA: {
-    type: DataTypes.STRING,
-    primaryKey: true
-  },
-  data_de_nascimento: DataTypes.DATE
-}, {
-  tableName: 'alunos',
-  timestamps: false
-});
+// Defina um modelo para a coleção alunos
+const Aluno = mongoose.model('Aluno', new mongoose.Schema({
+  nome: String,
+  RA: String,
+  data_de_nascimento: Date
+}));
 
 // Rota para criar um novo aluno
 app.post('/alunos', async (req, res) => {
@@ -32,22 +31,29 @@ app.post('/alunos', async (req, res) => {
 
 // Rota para obter todos os alunos
 app.get('/alunos', async (req, res) => {
-  const alunos = await Aluno.findAll();
+  let alunos = await Aluno.find().select('-_id -__v');
+  alunos = alunos.map(aluno => {
+    aluno = aluno.toObject();
+    aluno.data_de_nascimento = aluno.data_de_nascimento.toISOString().split('T')[0];
+    return aluno;
+  });
   res.json(alunos);
 });
 
 // Rota para atualizar um aluno
 app.put('/alunos/:RA', async (req, res) => {
-  const aluno = await Aluno.update(req.body, {
-    where: {
-      RA: req.params.RA
-    }
-  });
+  const aluno = await Aluno.findOneAndUpdate({ RA: req.params.RA }, req.body, { new: true });
+  res.json(aluno);
+});
+
+// Rota para remover um aluno
+app.delete('/alunos/:RA', async (req, res) => {
+  const aluno = await Aluno.findOneAndDelete({ RA: req.params.RA });
   res.json(aluno);
 });
 
 // Inicie o servidor
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
